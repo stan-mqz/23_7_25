99a9b3e3654cd802c4ecc941dee1178f5882b755
stanmqz <moscarstanley37@gmail.com>
2025-07-23 10:15:39 -0600

    Remove logicos.js from .gitignore

---
+++ logicos.js
@@ -0,0 +1,17 @@
+let edad = 20
+tieneDUI = true
+
+
+edad >= 18 && tieneDUI ? console.log('Puedes votar') : console.log('No puedes votar')
+
+
+let age = 45
+let esMiembro = false
+
+age >= 18 || esMiembro ? console.log('Tienes descuento') : console.log('No tienes descuento')
+
+let edad2 = 12
+let altura = 1.60
+
+let subir = edad >= 12 && altura >= 1.60 ? 'Si puede subir' : 'No puede subir'
+console.log(subir)
